103841ecd9d35c93c9f9a656e92d0ed11071cedb
Matteo Colucci <matitald@gmail.com>
2020-05-02 19:14:40 +0200

    Try to build ff extension with github action

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
 execSync(`yarn build firefox`);
-require('./sign-firefox');
-require('./update-page');
+// require('./sign-firefox');
+// require('./update-page');
 execSync('git add .');
