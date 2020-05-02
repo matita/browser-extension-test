6579a8c642f1e850a422378fdf5cf9ed21df3d5d
Matteo Colucci <matitald@gmail.com>
2020-05-02 18:20:41 +0200

    Update github pages with latest version download

--- scripts/sign-firefox.js
+++ scripts/sign-firefox.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const cheerio = require('cheerio');
 
+const { version } = require('../package.json');
 const sourceDir = path.resolve(__dirname, '../dist/firefox');
 const artifactsDir = path.resolve(__dirname, '../docs/builds/firefox/versions');
 const manifest = require('../dist/firefox/manifest.json');
@@ -15,3 +18,14 @@ execSync(
   ` --api-secret=${process.env.FIREFOX_API_SECRET}` +
   ` --id=${appId}`
 , { stdio: 'inherit' });
+
+const signedFileName = `browser-extension-test-${version}-an+fx.xpi`;
+const indexFilePath = path.resolve(__dirname, '../docs/index.html');
+const indexFileContent = '' + fs.readFileSync(indexFilePath);
+const $ = cheerio.load(indexFileContent);
+$('.download-ff')
+  .attr('href', `builds/firefox/versions/${signedFileName}`)
+  .text(`Install v${version} for Firefox`);
+const updatedFileContent = $.html();
+fs.readFileSync(indexFilePath, updatedFileContent);
+console.log('Updated link in GitHub Pages');
